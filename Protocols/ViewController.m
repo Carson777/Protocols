855d1456d3364e54ae1612534a9c8a76fe4234e5
Carson777 <carsoncapper@gmail.com>
2016-06-21 14:39:49 -0500

    Protocols Assignment, Week 2

--- Protocols/ViewController.m
+++ Protocols/ViewController.m
@@ -10,9 +10,60 @@
 
 @interface ViewController ()
 
+@property (nonatomic,weak) IBOutlet UISwitch *shouldBeginEditingSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *shouldEndEditingSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *charactersInRangeSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *shouldClearSwitch;
+@property (nonatomic,weak) IBOutlet UISwitch *shouldReturnSwitch;
+
+
 @end
 
 @implementation ViewController
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    if(self.shouldReturnSwitch.on){
+        NSLog(@"shouldReturnSwitch");
+    }
+    
+    return YES;
+    
+
+}
+- (BOOL)textFieldShouldClear:(UITextField *)textField{
+    
+    if(self.shouldClearSwitch.on){
+        NSLog(@"shouldClearSwitch");
+    }
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string{
+    
+    if(self.charactersInRangeSwitch.on){
+        NSLog(@"charactersInRangeSwitch");
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    
+    if(self.shouldEndEditingSwitch.on){
+        NSLog(@"shouldEndEditingSwitch");
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    
+    if(self.shouldBeginEditingSwitch.on){
+        NSLog(@"shouldBeginEditingSwitch");
+    }
+    return YES;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
